311092fd495f402e3943ff4b62afc12b9df540b0
Moises Martinez <l20590306@sjuanrio.tecnm.mx>
2025-02-11 22:16:10 -0600

    second

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hola_mundo():
     return 'Hola Mundo'
+@app.route('/alumnos')
+def getAlumnos():
+    return 'Aqui van los alumnos'
 
 if __name__ == '__main__':
     app.run(debug=True)
